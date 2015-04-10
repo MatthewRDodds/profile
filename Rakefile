ad8da11a7b4b7c6477464375a5c8aa20fae6483a
Matthew Dodds <matthewrusselldodds@gmail.com>
2015-04-10 13:12:55 -0400

    fix indention in application file

--- Rakefile
+++ Rakefile
@@ -1,18 +1,6 @@
 # %w{ bundler find rake/testtask}.each { |lib| require lib }
 # require 'bundler'
 
-task :default => 'docker:run'
-
-namespace :docker do
-	task :build do |t|
-		`docker build -t profile .`
-	end
-
-	task :run do |t|
-		`docker run -p 4567:80 --rm -it profile bash`
-	end
-end
-
 APP_FILE  = 'profile.rb'
 APP_CLASS = 'Sinatra::Application'
 
--- profile.rb
+++ profile.rb
@@ -2,13 +2,13 @@ require 'sinatra/base'
 require 'sinatra/partial'
 
 class Profile < Sinatra::Base
-	set public_folder: 'public', static: true
+  set public_folder: 'public', static: true
 
-	configure do
-	  register Sinatra::Partial
-	end
+  configure do
+    register Sinatra::Partial
+  end
 
-	get '/' do
-	  erb :index
-	end
+  get '/' do
+    erb :index
+  end
 end
